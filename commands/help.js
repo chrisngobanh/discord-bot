d6d194afd12a0bf3c6bec4c4a3fc1502ccf963b7
Chris Banh <chris@chrisbanh.com>
2022-07-23 00:50:03 -0700

    Fixes help command

--- commands/help.js
+++ commands/help.js
@@ -3,16 +3,17 @@ import fs from 'fs';
 export default {
   name: 'help',
   description: 'List all available commands.',
-  execute(interaction) {
+  execute: async (interaction) => {
+    await interaction.deferReply();
     let str = '';
     const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
     for (const file of commandFiles) {
-      const command = import(`./${file}`);
+	    const command = (await import(`./${file}`)).default;
       str += `\`/${command.name}\` - ${command.description} \n`;
     }
 
-    return void interaction.reply({
+    return void interaction.followUp({
       content: str,
       ephemeral: true,
     });
